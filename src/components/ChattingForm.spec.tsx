59670108ad72e153d141889196502b18f64ffb8f
JiyeonMin <ichbinmin2@gmail.com>
2022-03-26 18:08:56 +0900

    ✅  Add : ChattingForm 보내기 버튼 및 보내기 후 빈값 Test 추가

--- src/components/ChattingForm.spec.tsx
+++ src/components/ChattingForm.spec.tsx
@@ -17,7 +17,7 @@ describe("ChattingForm", () => {
     render(<ChattingForm dispatch={dispatch} />);
 
     // 입력란 => 멀티라인 => textarea
-    const chattingInput = screen.getByRole("textbox");
+    const chattingInput: HTMLInputElement = screen.getByRole("textbox");
     // 보내기 버튼
     const sendButton = screen.getByText("보내기");
 
@@ -36,7 +36,37 @@ describe("ChattingForm", () => {
     // expect 이렇게 될 거야
     expect(dispatch).toHaveBeenCalledWith(
       sent({
-        id: "test id 1",
+        id: now.valueOf().toString(),
+        userId: "jamong",
+        userName: "pingoo",
+        profileImage: "https://image.jpeg", // 이미지 주소
+        content: "test chatting",
+        date: now.toLocaleString(),
+      })
+    );
+
+    expect(chattingInput.value).toBe("")
+  });
+
+  it("값을 입력해야만 보내기 버튼을 눌러 채팅을 전송할 수 있다", async () => {
+    // render
+    const dispatch = jest.fn();
+    render(<ChattingForm dispatch={dispatch} />);
+
+    // 입력란 => 멀티라인 => textarea
+    const chattingInput: HTMLInputElement = screen.getByRole("textbox");
+
+    // 메세지를 타이핑을 한다
+    // 엔터 키를 누르면
+    userEvent.type(chattingInput, "test chatting{enter}"); // 
+    const now = new Date("2021-03-14");
+    jest.setSystemTime(now);
+
+    // 잘 들어간다???
+    // expect 이렇게 될 거야
+    expect(dispatch).toHaveBeenCalledWith(
+      sent({
+        id: now.valueOf().toString(),
         userId: "jamong",
         userName: "pingoo",
         profileImage: "https://image.jpeg", // 이미지 주소
