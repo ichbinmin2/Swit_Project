848ef57542e43234582453d7a7ce3f5d11a97a0c
JiyeonMin <ichbinmin2@gmail.com>
2022-03-26 18:10:48 +0900

    ✅  Add : ChattingForm submit, eventHandler 추가

--- src/components/ChattingForm.tsx
+++ src/components/ChattingForm.tsx
@@ -9,23 +9,40 @@ const ChattingForm = ({ dispatch }) => {
     setInput(e.target.value);
   }
 
+  function sumbit(){
+    dispatch(
+      sent({
+        id: Date.now().toString(),
+        userId: "jamong",
+        userName: "pingoo",
+        profileImage: "https://image.jpeg", // 이미지 주소
+        content: input.trim(),
+        date: new Date().toLocaleString(),
+      })
+    );
+
+    setInput("");
+  }
+
+  function handleKeyUp(e: React.KeyboardEvent<HTMLTextAreaElement>) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      sumbit(); 
+    }
+  }
+
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
-        dispatch(
-          sent({
-            id: "test id 1",
-            userId: "jamong",
-            userName: "pingoo",
-            profileImage: "https://image.jpeg", // 이미지 주소
-            content: input,
-            date: new Date().toLocaleString(),
-          })
-        );
+        sumbit();
       }}
     >
-      <textarea value={input} onChange={handleChange}></textarea>
+      <textarea
+        value={input}
+        onChange={handleChange}
+        onKeyUp={handleKeyUp}
+      ></textarea>
       <button type="submit" disabled={disabled}>
         보내기
       </button>
