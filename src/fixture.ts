b199a78ea827e149c3de56885cf6644359d4a7b4
JiyeonMin <ichbinmin2@gmail.com>
2022-03-26 18:13:57 +0900

    ✅  Add : Test message data 추가 및 type 지정

---
+++ src/fixture.ts
@@ -0,0 +1,18 @@
+// fixture
+export const TEST_MESSAGE = {
+  id: "test id 1",
+  userId: "apopo",
+  userName: "jamong",
+  profileImage: "https://image.jpeg", // 이미지 주소
+  content: "test test test",
+  date: "2022-03-14 19:28:29",
+};
+
+export const TEST_MESSAGE_2 = {
+  id: "test id 2",
+  userId: "antpenguin",
+  userName: "pingoo",
+  profileImage: "https://image.jpeg", // 이미지 주소
+  content: "test test 2222",
+  date: "2022-03-14 19:30:29",
+};
---
+++ src/index.d.ts
@@ -0,0 +1,8 @@
+type Message = {
+  id: string;
+  userId: string;
+  userName: string;
+  profileImage: string; // 이미지 주소
+  content: string;
+  date: string; // yyyy-mm-dd hh:MM:ss
+};
